ed765ec8fb3a6a5bdd0f27a9435e300ea8c9adec
Ezidol <goodyoungmin@naver.com>
2020-12-15 21:30:41 +0900

    10951

---
+++ 10951.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    while(1) {
+        if (scanf("%d %d", &a, &b) == -1) {
+            break;
+        }
+        if (a == -1) {
+            break;
+        }
+        printf("%d\n", a+b);
+        
+    }
+    return 0;
+}
